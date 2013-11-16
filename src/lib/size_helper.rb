81f6d08ac502f8566bf3f8fb1dd2db266ce6215e
Josef Cech <czech.josef@gmail.com>
2013-11-16 13:20:33 +0100

    size_helper: Can handle relative sizes

--- src/lib/size_helper.rb
+++ src/lib/size_helper.rb
@@ -4,14 +4,15 @@ class SizeHelper
     sum = 0
     flexible_nodes = []
     nodes.each do |node|
-      sum += node.size
+      node_size = node.size > 1 ? node.size : (max_size * node.size).to_i
+      sum += node_size
       raise RangeError, 'Sum exceeds window/pane size ' + max_size.to_s if sum > max_size
 
-      node.absolute_size = node.size
+      node.absolute_size = node_size
       flexible_nodes.push node if node.size == Window::Node::FLEXIBLE_SIZE
     end
 
-    if flexible_nodes.count
+    if flexible_nodes.count > 0
       part = (max_size - sum) / flexible_nodes.count
       flexible_nodes.each { |node| node.absolute_size = part }
     end
--- test/suite/size_helper_test.rb
+++ test/suite/size_helper_test.rb
@@ -18,11 +18,25 @@ class TestSizeHelper < Test::Unit::TestCase
     assert_equal 50, nodes[1].absolute_size
   end
 
-  def test_sum_size_greater_than_max_size_should_raise_error
+  def test_sum_size_greater_than_max_size__should_raise_error
     nodes = [ Window::Pane.new(nil, 10), Window::Pane.new(nil, 30) ]
     error = assert_raise RangeError do
       SizeHelper.fill_absolute_sizes! 20, nodes
     end
     assert_equal 'Sum exceeds window/pane size 20', error.message
+  end
+
+  def test_two_panes__first_percentage_second_flex
+    nodes = [ Window::Pane.new(nil, 0.3), Window::Pane.new(nil, Window::Node::FLEXIBLE_SIZE) ]
+    SizeHelper.fill_absolute_sizes! 100, nodes
+    assert_equal 30, nodes[0].absolute_size
+    assert_equal 70, nodes[1].absolute_size
+  end
+
+  def test_two_panes__first_abs_second_abs__should_not_raise_zero_division_error
+    nodes = [ Window::Pane.new(nil, 10), Window::Pane.new(nil, 20) ]
+    SizeHelper.fill_absolute_sizes! 100, nodes
+    assert_same 10, nodes[0].absolute_size
+    assert_same 20, nodes[1].absolute_size
   end
 end
